d264ab617d35e88649dc4c0400f459acec736bbe
prochazk <prochazk@OA-S141-21>
2024-01-10 11:05:23 -0500

    Added keyboard input to the tank

---
+++ Assets/Week 1/Scripts/Mover.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+
+    public float speed = 3f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float direction = Input.GetAxis("Horizontal");
+        
+        transform.Translate(speed * direction * Time.deltaTime, 0, 0);
+        
+
+     
+
+    }
+}
