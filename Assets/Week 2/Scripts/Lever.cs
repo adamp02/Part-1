5cd7a73a842489aceeea3899cbed5bed86d93252
prochazk <prochazk@OA-S141-21>
2024-01-17 11:32:11 -0500

    Task 4B - Working lever & door system

---
+++ Assets/Week 2/Scripts/Lever.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+
+    public GameObject door;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (door.activeInHierarchy)
+        {
+            door.SetActive(false);
+        } else
+        {
+            door.SetActive(true);
+        }
+    }
+}
