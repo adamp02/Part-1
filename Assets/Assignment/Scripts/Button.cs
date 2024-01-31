98840a3ec0351d98aed855492432b271a676ace9
AdamP <62314569+adamp02@users.noreply.github.com>
2024-01-30 20:18:49 -0500

    Added button / collision and instantiation
    Added a button that spawns cargo for the player

---
+++ Assets/Assignment/Scripts/Button.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+
+    public GameObject cargo;
+    public Transform cargoSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Button activated!");
+        Instantiate(cargo, cargoSpawn.position, cargoSpawn.rotation);
+
+    }
+
+
+}
